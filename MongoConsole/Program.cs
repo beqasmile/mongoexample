408c439fe12486a8028f8f6f285cebaa82cf9c02
ilya <beqasmile@gmail.com>
2020-10-28 20:00:06 +0200

    insert and first select

--- MongoConsole/Program.cs
+++ MongoConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +13,52 @@ namespace MongoConsole
         static void Main(string[] args)
         {
 
-            MongoClient dbClient = new MongoClient("mongodb+srv://XXX:XXXXXXXXX@cluster0.oyqyt.mongodb.net/sample_airbnb?retryWrites=true&w=majority");
+            MongoClient dbClient = new MongoClient("mongodb+srv://ilya:Ia1234567890@cluster0.oyqyt.mongodb.net/sample_airbnb?retryWrites=true&w=majority");
 
             var dbList = dbClient.ListDatabases().ToList();
 
             Console.WriteLine("The list of databases on this server is: ");
-            foreach (var db in dbList)
-            {
-                Console.WriteLine(db);
-            }
+
+            var database = dbClient.GetDatabase("sample_training");
+            var collection = database.GetCollection<BsonDocument>("grades");
+
+            var document = new BsonDocument { { "student_id", 10000 }, {
+                "scores",
+                new BsonArray {
+                new BsonDocument { { "type", "examilya" }, { "score", 88.12334193287023 } },
+                new BsonDocument { { "type", "quizily" }, { "score", 74.92381029342834 } },
+                new BsonDocument { { "type", "homework" }, { "score", 89.97929384290324 } },
+                new BsonDocument { { "type", "homework" }, { "score", 82.12931030513218 } }
+                }
+                }, { "class_id", 480 }
+        };
+
+            collection.InsertOne(document);
+
+
+            var firstDocument = collection.Find(new BsonDocument()).FirstOrDefault();
+            Console.WriteLine(firstDocument.ToString());
+
+
+
+            //foreach (var db in dbList)
+            //{
+            //    var dbName =db.GetElement("name");
+            //    var database1 = dbClient.GetDatabase(dbName.ToString());
+
+            //    var collection1 = database.GetCollection<Review>("listingsAndReviews");
+
+            //    var filterDef = new FilterDefinitionBuilder<Review>();
+            //    var filter = filterDef.In(x => x._id, new[] { "10006546", "10009999" });
+            //    //var reviewList = collection.Find(filter).ToList();
+
+            //    var empList = collection1.Find(filter).ToList();
+
+            //   // var firstDocument = collection1.Find(new BsonDocument()).FirstOrDefault();
+            //    Console.WriteLine(firstDocument.ToString());
+
+            //    Console.WriteLine(db);
+            //}
 
             Console.ReadKey();
         }
---
+++ MongoConsole/Review.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoConsole
+{
+    public class Review
+    {
+        public string _id;
+        public string _listing_url;
+        public string summary;
+        public string space;
+        public string description;
+    }
+}
