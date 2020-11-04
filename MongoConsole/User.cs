014b2b9de9b4de4665a05dc4eb2012b4506d78bc
ilya <beqasmile@gmail.com>
2020-11-04 20:12:03 +0200

    user

---
+++ MongoConsole/User.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoConsole
+{
+    public class User
+    {
+        [BsonElement("Id")]
+        public int Id { get; set; }
+
+        [BsonElement("UserName")]
+        public string UserName { get; set; }
+
+        [BsonElement("UserFamily")]
+        public string UserFamily { get; set; }
+
+        [BsonElement("UserAddress")]
+        public string UserAddress { get; set; }
+
+    }
+}
